5cd8f3244059c0ec99752aeb93e7805ad9c53a08
Sebastian Sanchez <sgeilp55@gmail.com>
2020-12-15 10:11:19 -0500

    prueba

---
+++ database/db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE crudnodejsmysql;
+
+use crudnodejsmysql;
+
+CREATE TABLE customer(
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+
+SHOW TABLES;
+
+describe customer;
